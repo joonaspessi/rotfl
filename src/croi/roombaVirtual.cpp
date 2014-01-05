93dd1172259577fd96699d74816093c7370650b3
JuhaniJ <juhani87@gmail.com>
2014-01-05 18:12:40 +0200

    VIRTUAL_ROOMBA: Added the new files to git (forgot before)

---
+++ src/croi/roombaVirtual.cpp
@@ -0,0 +1,180 @@
+#include "roombaVirtual.h"
+#include "croiUtil.h"
+#include "mapQGraphicsView.h"
+#include <QDebug>
+
+namespace Croi {
+
+RoombaVirtual::RoombaVirtual(PoiQGraphicsEllipseItem *startPoint,
+                             MapQGraphicsView* map, FleetManager *parent):
+    IRoomba(startPoint, map, parent), deltaDistance_(0.0), deltaAngle_(0.0),
+    lastDriveCmdTime_(0)
+{
+    statusTimer_ = new QTimer(this);
+    IRoomba::updateState(); //call needed to have no null pointer in icon_
+}
+
+RoombaVirtual::~RoombaVirtual()
+{
+}
+
+int RoombaVirtual::rmb_connect(std::string ip)
+{
+    connect(statusTimer_, SIGNAL(timeout()), this, SLOT(statusTimerTimeout()));
+    statusTimer_->setSingleShot(false);
+    statusTimer_->start(30);
+}
+
+int RoombaVirtual::disconnect()
+{
+    statusTimer_->stop();
+    IRoomba::disconnect();
+}
+
+void RoombaVirtual::allMotorsOn()
+{
+}
+
+void RoombaVirtual::allMotorsOff()
+{
+}
+
+void RoombaVirtual::clean()
+{
+}
+
+char RoombaVirtual::getTemperature()
+{
+    return 0;
+}
+
+unsigned short RoombaVirtual::getChargeLevel()
+{
+    return 0;
+}
+
+double RoombaVirtual::getDistance()
+{
+    double tmp = deltaDistance_;
+    deltaDistance_ = 0.0;
+    return tmp;
+}
+
+double RoombaVirtual::getAngle()
+{
+    double tmp = deltaAngle_;
+    deltaAngle_ = 0.0;
+    return tmp;
+}
+
+bool RoombaVirtual::getLeftBumb()
+{
+    return false;
+}
+
+bool RoombaVirtual::getRightBumb()
+{
+    return false;
+}
+
+void RoombaVirtual::drive(int velocity, int radius)
+{
+    if(IRoomba::getVelocity() != 0)
+    {
+        //previous movement needs to be recorded
+        switch (IRoomba::getRadius()) {
+        case Util::RADSTRAIGHT:
+            deltaDistance_ -= (statusTimer_->interval()-statusTimer_->remainingTime()-lastDriveCmdTime_)*IRoomba::getVelocity()/1000.0/DISTANCECORRECTION;
+            break;
+        case Util::RADTURNCW:
+            deltaAngle_ -= (statusTimer_->interval()-statusTimer_->remainingTime()-lastDriveCmdTime_)*IRoomba::getVelocity()/100.0/(TURNTIMEINUSFORDEG/1000.0)/ANGLECORRECTION;
+            break;
+        case Util::RADTURNCCW:
+            deltaAngle_ += (statusTimer_->interval()-statusTimer_->remainingTime()-lastDriveCmdTime_)*IRoomba::getVelocity()/100.0/(TURNTIMEINUSFORDEG/1000.0)/ANGLECORRECTION;
+            break;
+        default:
+              //Below code for radial movement doesn't work
+//            double deltaDeltaDistance = (statusTimer_->interval()-statusTimer_->remainingTime()-lastDriveCmdTime_)*IRoomba::getVelocity()/DISTANCECORRECTION;
+//            deltaDistance_ -= deltaDeltaDistance;
+//            deltaAngle_ -= deltaDeltaDistance/(IRoomba::getRadius()*2*Util::PI);
+            break;
+        }
+    }
+    lastDriveCmdTime_ = statusTimer_->interval()-statusTimer_->remainingTime();
+    IRoomba::drive(velocity, radius);
+}
+
+void RoombaVirtual::drive(int velocity)
+{
+    if(IRoomba::getVelocity() != 0)
+    {
+        //previous movement needs to be recorded
+        switch (IRoomba::getRadius()) {
+        case Util::RADSTRAIGHT:
+            deltaDistance_ -= (statusTimer_->interval()-statusTimer_->remainingTime()-lastDriveCmdTime_)*IRoomba::getVelocity()/1000.0/DISTANCECORRECTION;
+            break;
+        case Util::RADTURNCW:
+            deltaAngle_ -= (statusTimer_->interval()-statusTimer_->remainingTime()-lastDriveCmdTime_)*IRoomba::getVelocity()/100.0/(TURNTIMEINUSFORDEG/1000.0)/ANGLECORRECTION;
+            break;
+        case Util::RADTURNCCW:
+            deltaAngle_ += (statusTimer_->interval()-statusTimer_->remainingTime()-lastDriveCmdTime_)*IRoomba::getVelocity()/100.0/(TURNTIMEINUSFORDEG/1000.0)/ANGLECORRECTION;
+            break;
+        default:
+              //Below code for radial movement doesn't work
+//            double deltaDeltaDistance = (statusTimer_->interval()-statusTimer_->remainingTime()-lastDriveCmdTime_)*IRoomba::getVelocity()/DISTANCECORRECTION;
+//            deltaDistance_ -= deltaDeltaDistance;
+//            deltaAngle_ -= deltaDeltaDistance/(IRoomba::getRadius()*2*Util::PI);
+            break;
+        }
+    }
+    lastDriveCmdTime_ = statusTimer_->interval()-statusTimer_->remainingTime();
+    IRoomba::drive(velocity);
+}
+
+void RoombaVirtual::playSong(int songNumber)
+{
+}
+
+void RoombaVirtual::statusTimerTimeout()
+{
+    if(IRoomba::getVelocity() != 0)
+    {
+        //previous movement needs to be recorded
+        switch (IRoomba::getRadius()) {
+        case Util::RADSTRAIGHT:
+            deltaDistance_ -= (statusTimer_->interval()-lastDriveCmdTime_)*IRoomba::getVelocity()/1000.0/DISTANCECORRECTION;
+            break;
+        case Util::RADTURNCW:
+            deltaAngle_ -= (statusTimer_->interval()-lastDriveCmdTime_)*IRoomba::getVelocity()/100.0/(TURNTIMEINUSFORDEG/1000.0)/ANGLECORRECTION;
+            break;
+        case Util::RADTURNCCW:
+            deltaAngle_ += (statusTimer_->interval()-lastDriveCmdTime_)*IRoomba::getVelocity()/100.0/(TURNTIMEINUSFORDEG/1000.0)/ANGLECORRECTION;
+            break;
+        default:
+              //Below code for radial movement doesn't work
+//            double deltaDeltaDistance = (statusTimer_->interval()-lastDriveCmdTime_)*IRoomba::getVelocity()/DISTANCECORRECTION;
+//            deltaDistance_ -= deltaDeltaDistance;
+//            deltaAngle_ -= deltaDeltaDistance/(IRoomba::getRadius()*2*Util::PI);
+            break;
+        }
+    }
+    lastDriveCmdTime_ = 0;
+    IRoomba::sensorUpdateTimerTimeout();
+    //these currently nulled in get-functions
+    //deltaDistance_ = 0.0;
+    //deltaAngle_ = 0.0;
+}
+
+QGraphicsPixmapItem* RoombaVirtual::setIcon()
+{
+    // TODO: Improve icon graphics before deploying
+    QPixmap pixmap(":/icons/DEBUG_roomba_virtual_small");
+    QGraphicsPixmapItem* icon = IRoomba::getMap()->scene()->addPixmap(pixmap);
+    icon->setOffset(Util::ROOMBAWIDTH/-2.0, Util::ROOMBAWIDTH/-2.0);
+    icon->setPos(IRoomba::getLoc().x(), IRoomba::getLoc().y());
+    icon->setFlag(QGraphicsItem::ItemIsSelectable, true);
+    icon->setFlag(QGraphicsItem::ItemIsMovable,false);
+    return icon;
+}
+
+} //namespace Croi
---
+++ src/croi/roombaVirtual.h
@@ -0,0 +1,49 @@
+#ifndef ROOMBAVIRTUAL_H
+#define ROOMBAVIRTUAL_H
+
+#include "iRoomba.h"
+#include <QTimer>
+#include <QGraphicsPixmapItem>
+
+namespace Croi {
+
+class RoombaVirtual : public IRoomba
+{
+    Q_OBJECT
+
+public:
+    RoombaVirtual(PoiQGraphicsEllipseItem *startPoint, MapQGraphicsView* map,
+                  FleetManager *parent = NULL);
+    virtual ~RoombaVirtual();
+
+    virtual int rmb_connect(std::string ip);
+    virtual int disconnect();
+    virtual void allMotorsOn();
+    virtual void allMotorsOff();
+    virtual void clean();
+    virtual void drive( int velocity, int radius );
+    virtual void drive( int velocity);
+    virtual void playSong( int songNumber );
+    virtual char getTemperature();
+    virtual unsigned short getChargeLevel();
+
+    virtual double getDistance();
+    virtual double getAngle();
+    virtual bool getLeftBumb();
+    virtual bool getRightBumb();
+    virtual QGraphicsPixmapItem* setIcon();
+
+signals:
+
+public slots:
+    void statusTimerTimeout();
+
+private:
+    QTimer* statusTimer_;
+    double deltaDistance_;
+    double deltaAngle_;
+    int lastDriveCmdTime_;
+};
+
+} //namespace Croi
+#endif // ROOMBAVIRTUAL_H
